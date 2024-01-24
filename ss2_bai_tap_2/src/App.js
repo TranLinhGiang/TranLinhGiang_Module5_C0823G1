aee076bac906903398c1547dadf1af020134e113
Trần Linh Giang <gt07839@gmail.com>
2024-01-24 14:11:57 +0700

    Giang_nop_bai_tap_2_ss2

---
+++ ss2_bai_tap_2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signin from './components/Signin';
+
+function App() {
+  return (
+    <div className="App">
+     <Signin/>
+    </div>
+  );
+}
+
+export default App;
