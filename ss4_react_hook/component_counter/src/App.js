5098d65d4b9c8cf57bcc8eaa61f402b05c8d0472
Trần Linh Giang <gt07839@gmail.com>
2024-01-25 20:10:37 +0700

    Nộp bài tập ss4

---
+++ ss4_react_hook/component_counter/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
