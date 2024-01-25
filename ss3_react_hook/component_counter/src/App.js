d49862fe31e6fd1bf6beeceae25d7812bc677e8c
Trần Linh Giang <gt07839@gmail.com>
2024-01-25 20:06:44 +0700

    Nộp bài tập ss4

--- ss3_react_hook/component_counter/src/App.js
+++ ss3_react_hook/component_counter/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
--- ss3_react_hook/component_counter/src/components/Counter.js
+++ ss3_react_hook/component_counter/src/components/Counter.js
@@ -1,18 +1,29 @@
 import { useState } from "react";
-
+import "./Counter.css"
 function Counter(){
     const [countOne,setCounOne]= useState(0);
     const [countTwo,setCounTwo]= useState(0);
     const click = (countOne) => setCounOne(prev=> prev+1) 
     const clicks = (countTwo) => setCounTwo(prev=> prev+2) 
+    const clickss = () =>{
+        setCounOne (0)
+        setCounTwo (0)
+    }
     return(
         <>
+        <div className="my-container">
         <h1>Bộ đếm</h1>
-        <h1>Count :{countOne} </h1>
-        <button onClick={click}>Add 1</button>
+        <h1>Count :{countOne}</h1>
+        <button onClick={click} className="btn btn-success">Add 1</button>
         <h1>Count :{countTwo}</h1>
-        <button onClick={clicks}>Add 2</button>
+        <button onClick={clicks} className="btn btn-success">Add 2</button> 
+        <br />
+        <br />
+        <br />
+        <button onClick={clickss} className="btn btn-success">clean up</button>
+        </div>   
         </>
     )
+ 
 }
 export default Counter;
