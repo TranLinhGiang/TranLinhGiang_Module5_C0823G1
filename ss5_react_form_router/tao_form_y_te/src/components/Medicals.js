8726dcf5fe98fa055a45db767630d493edd95811
Trần Linh Giang <gt07839@gmail.com>
2024-01-28 14:17:07 +0700

    nộp bài tập ss5

--- ss5_react_form_router/tao_form_y_te/src/components/Medicals.js
+++ ss5_react_form_router/tao_form_y_te/src/components/Medicals.js
@@ -3,9 +3,12 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormGroup from '@mui/material/RadioGroup';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import { Field, Form, Formik, useFormik } from 'formik';
+import { Formik, Form, Field,useFormik, ErrorMessage } from 'formik';
+import { ToastContainer, toast } from 'react-toastify';
 import "./Medicals.css"
-function Medicals(){ 
+import React from 'react';
+import * as Yup from 'yup';
+function Medicals(){
     const formMedicals= useFormik(
         {
             initialValues:{
@@ -16,7 +19,7 @@ function Medicals(){
               quốc_tịch:"",
               công_ty: "",
               bộ_phận_làm_việc: "",
-              thẻ_bảo_hiểm_y_tế: {},
+              thẻ_bảo_hiểm_y_tế: [],
               tỉnh_thành: "",
               quận_huyện: "",
               phường_xã: "",
@@ -24,35 +27,66 @@ function Medicals(){
               số_điện_thoại: "",
               email: "",
               câu_hỏi_số_1:"",
-              câu_hỏi_số_2: {},
-              câu_hỏi_số_3: {},
+              câu_hỏi_số_2: [],
+              câu_hỏi_số_3: [],
                 
             },
-            onSubmit: values => {
-                alert(JSON.stringify(values,null,2));
-            }
+            onSubmit:( values, setSubmitting) => {
+              setTimeout(() => {
+                toast("Thêm mới thành công")
+                alert(JSON.stringify(values,null,2))
+              },2000)
+                
+            },
+
         }
     );
-  return(
+    return(
     <>
     <div className='container hi'>
      <h1>Tờ khai y tế</h1>
- <Formik>
+ <Formik
+  validationSchema={Yup.object({
+    họ_và_tên:Yup.string()
+    .required('vui lòng nhập họ và tên'),
+    số_hộ_chiếu_cmnd:Yup.string()
+    .required('vui lòng nhập số hộ chiếu'),
+    năm_sinh:Yup.number('vui lòng nhập số')
+    .min(1990),
+    quốc_tịch:Yup.string()
+    .required('vui lòng nhập quốc tịch'),
+    tỉnh_thành:Yup.string()
+    .required('vui lòng nhập tỉnh thành'),
+    quận_huyện:Yup.string()
+    .required('vui lòng nhập quận huyện'),
+    phường_xã: Yup.string()
+    .required('vui lòng nhập phường, xã'),
+    số_điện_thoại:Yup.string()
+    .required('vui lòng nhập số điện thoại'),
+    email:Yup.string()
+    .required('vui lòng nhập email')
+    .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+  })}
+ >
 
      <Form onSubmit={formMedicals.handleSubmit}>
         
      <label>Họ và tên</label><br/>
       <Field name='họ_và_tên'value={formMedicals.values.họ_và_tên} onChange={formMedicals.handleChange}></Field><br/>
+      <ErrorMessage name='họ_và_tên' component='span' className='erros_message'/>
 
      <label>Số hộ chiếu/CMND</label><br/>
       <Field name='số_hộ_chiếu_cmnd' value={formMedicals.values.số_hộ_chiếu_cmnd} onChange={formMedicals.handleChange}></Field><br/>
+      <ErrorMessage name='số_hộ_chiếu_cmnd' component='span' className='erros_message'/>
 
      <label>Năm sinh</label><br/>
       <Field name='năm_sinh'value={formMedicals.values.năm_sinh} onChange={formMedicals.handleChange}></Field><br/>
+      <ErrorMessage name='năm_sinh' component='span' className='erros_message'/>
 
      <label>Giới tính</label>
      <RadioGroup
         aria-labelledby="demo-radio-buttons-group-label"
+
         defaultValue="female"
         name='giới_tính'>
         <FormControlLabel value="Nam" onChange={formMedicals.handleChange} control={<Radio />} label="Nam" />
@@ -61,6 +95,7 @@ function Medicals(){
 
       <label>Quốc tịch</label><br/>
        <Field name='quốc_tịch'value={formMedicals.values.quốc_tịch} onChange={formMedicals.handleChange}></Field><br/>
+       <ErrorMessage name='quốc_tịch' component='span' className='erros_message'/>
 
       <label>Công ty làm việc</label><br/>
        <Field name='công_ty'value={formMedicals.values.công_ty} onChange={formMedicals.handleChange}></Field><br/>
@@ -79,21 +114,28 @@ function Medicals(){
 
       <label>Tỉnh thành</label><br/>
        <Field name='tỉnh_thành' value={formMedicals.values.tỉnh_thành} onChange={formMedicals.handleChange}></Field><br/>
+       <ErrorMessage name='tỉnh_thành' component='span' className='erros_message'/>
 
       <label>Quận/huyện</label><br/>
        <Field name='quận_huyện' value={formMedicals.values.quận_huyện} onChange={formMedicals.handleChange}></Field><br/>
+       <ErrorMessage name='quận_huyện' component='span' className='erros_message'/>
 
       <label>Phường/xã</label><br/>
        <Field name='phường_xã' value={formMedicals.values.phường_xã} onChange={formMedicals.handleChange}></Field><br/>
+       <ErrorMessage name='phường_xã' component='span' className='erros_message'/>
 
       <label>Số nhà, phố, tổ dân phố/ thôn/ đội</label><br/>
        <Field name='số_nhà' value={formMedicals.values.số_nhà} onChange={formMedicals.handleChange}></Field><br/>
+       <ErrorMessage name='số_nhà' component='span' className='erros_message'/>
 
       <label>Điện thoại</label><br/>
        <Field name='số_điện_thoại' value={formMedicals.values.số_điện_thoại} onChange={formMedicals.handleChange}></Field><br/>
+       <ErrorMessage name='số_điện_thoại' component='span' className='erros_message'/>
+
 
       <label>Email</label><br/>
        <Field name='email' value={formMedicals.values.email} onChange={formMedicals.handleChange}></Field><br/>
+       <ErrorMessage name='email' component='span' className='erros_message'/>
 
       <h4>Trong vòng 14 ngày qua ,anh/chị <br/>có đến quốc gia/vùng lãnh thổ nào <br/>(Có thể đi qua nhiều quốc gia)</h4>
       <textarea name='câu_hỏi_số_1' onChange={formMedicals.handleChange}></textarea><br/>
@@ -118,6 +160,7 @@ function Medicals(){
      </Form>
      </Formik>
      </div>
+     <ToastContainer/>
     </>
   )
 }
