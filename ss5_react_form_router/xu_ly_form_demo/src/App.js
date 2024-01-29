69b64c9525c765a93624550b07e69e9e1ebfcc71
Trần Linh Giang <gt07839@gmail.com>
2024-01-29 10:11:51 +0700

    Nộp bài tập ss5

---
+++ ss5_react_form_router/xu_ly_form_demo/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FormCreate from './components/Form';
+import React from "react";
+
+function App() {
+return (
+<div className="App">
+  <FormCreate />
+</div>
+);
+}
+
+export default App;
